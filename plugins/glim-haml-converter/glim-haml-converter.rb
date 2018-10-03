8aa4291a99f2ee6a1a4363dd4eccb54663a353be
Allan Odgaard <git@abetarda.com>
2018-10-03 09:28:01 +0200

    Extend HAML support to also include layouts
    When setting layout to ‘page’ in a file’s front matter then this filter will check if there is any ‘$layouts_dir/page.haml’, if not, we default to the regular layout filter (which will use the liquid template engine).

--- plugins/glim-haml-converter/glim-haml-converter.rb
+++ plugins/glim-haml-converter/glim-haml-converter.rb
@@ -33,4 +33,37 @@ module GlimHAMLSupport
       content = engine.render(ExposeLiquidFilters.new(@site, page), :content => content, :page => ExposeLiquidGetterAPI.new(page), :site => ExposeLiquidGetterAPI.new(@site))
     end
   end
+
+  class HAMLLayout < Glim::Filter
+    transforms '*' => 'output'
+
+    def initialize(site)
+      @site  = site
+      @cache = {}
+    end
+
+    def find_layout(name)
+      unless name.nil? || @cache.has_key?(name)
+        path = File.join(@site.layouts_dir, name + '.haml')
+        @cache[name] = if File.exists?(path)
+          Glim::FileItem.new(@site, path)
+        end
+      end
+      @cache[name]
+    end
+
+    def transform(content, page, options)
+      layout = page.data['layout']
+      if find_layout(layout)
+        while layout_file = find_layout(layout)
+          engine  = Haml::Engine.new(layout_file.content('liquid'))
+          content = engine.render(ExposeLiquidFilters.new(@site, page), :content => content, :page => ExposeLiquidGetterAPI.new(page), :site => ExposeLiquidGetterAPI.new(@site))
+          layout  = layout_file.data['layout']
+        end
+        content
+      else
+        super
+      end
+    end
+  end
 end
